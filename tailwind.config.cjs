ec08949a33068f2dd38540c1c1398b91ab01bb41
Ryan Sousa <Ryan.sousa4000@gmail.com>
2023-02-28 09:49:14 -0300

    Alterações visuais na tela de login

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "random": 'url("https://source.unsplash.com/random")',
+      },
       colors: {
         primary: {
           200: "#a4fc4c",
@@ -13,6 +16,9 @@ module.exports = {
           700: "#528026",
           800: "#466e20",
           900: "#385919",
+          yellow: "#C1D602",
+          green: "#1B7A24",
+          blue: "#1B4D82",
         },
       },
     },
